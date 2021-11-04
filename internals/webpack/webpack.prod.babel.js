1d8e7aa3989d516a1c4348c9e97f104b1ce2281f
Krishna Yadav <info@w3turbo.com>
2021-11-04 11:50:34 +0530

    fix new

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -128,17 +128,17 @@ module.exports = require('./webpack.base.babel')({
       theme_color: '#b1624d',
       inject: true,
       ios: true,
-      // icons: [
-      //   {
-      //     src: path.resolve('public/images/logo.png'),
-      //     sizes: [72, 96, 128, 144, 192, 384, 512],
-      //   },
-      //   {
-      //     src: path.resolve('public/images/logo.png'),
-      //     sizes: [120, 152, 167, 180],
-      //     ios: true,
-      //   },
-      // ],
+      icons: [
+        {
+          src: path.resolve('public/images/logo.png'),
+          sizes: [72, 96, 128, 144, 192, 384, 512],
+        },
+        {
+          src: path.resolve('public/images/logo.png'),
+          sizes: [120, 152, 167, 180],
+          ios: true,
+        },
+      ],
     }),
 
     new HashedModuleIdsPlugin({
